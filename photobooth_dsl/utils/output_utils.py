49b1a1c9414a2b4e3dbcf5efa06739a2db6aba5a
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-07-17 18:55:34 -0500

    feat: add function to get the `original` or `modified` images folder

--- photobooth_dsl/utils/output_utils.py
+++ photobooth_dsl/utils/output_utils.py
@@ -9,3 +9,13 @@ def check_format(file_name):
         return True
     else:
         return False
+
+
+# The value argument is an image path or a variable
+def get_images_folder(value, folder):
+    if folder == "original":
+        return "photobooth_dsl/images/original/" + value
+    elif folder == "modified":
+        return "photobooth_dsl/images/modified_images/" + value
+    else:
+        raise ValueError("Invalid folder type specified. Choose 'original' or 'modified'.")
