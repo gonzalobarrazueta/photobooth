ddde50c3e085c413aa1892b9f1b88d1512653e20
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-07-17 19:59:11 -0500

    fix: identify string values if they are enclosed in single quotes

--- photobooth_dsl/utils/variables_util.py
+++ photobooth_dsl/utils/variables_util.py
@@ -1,8 +1,8 @@
 # Handles cases where the value passed is a variable
 def is_variable(value):
 
-    # If the value is enclosed in double quotes, it is a string and not a variable
-    if value.startswith('"') and value.endswith('"'):
+    # If the value is enclosed in quotes, it is a string and not a variable
+    if value.startswith("'") and value.endswith("'"):
         return False
     else:
         return True
